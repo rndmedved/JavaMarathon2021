ca4361b22465e475b51623d014bc5d1132a31440
Mikhail_Kuragin <rndmku@gmail.com>
2021-01-21 17:03:31 +0700

    Add Day2 solutions

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,20 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int floor = scanner.nextInt();
+        scanner.close();
+        if (floor < 1) {
+            System.out.println("Ошибка ввода");
+        }else if (floor < 5) {
+            System.out.println("Малоэтажный дом");
+        }else if(floor < 9){
+            System.out.println("Среднеэтажный дом");
+        } else {
+            System.out.println("Многоэтажный дом");
+        }
     }
 }
--- src/main/java/day2/Task4.java
+++ src/main/java/day2/Task4.java
@@ -1,7 +1,19 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task4 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        double x = scanner.nextDouble();
+        scanner.close();
+        System.out.println(y(x));
+    }
+    public static double y(double x){
+        if (x >= 5) {
+            return (Math.pow(x, 2) - 10) / (x + 7);
+        } else if (x > -3 && x < 5){
+            return (x + 3) * (Math.pow(x, 2) - 2);
+        } else  return 420;
     }
 }
