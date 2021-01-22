3df8031c8fcc39a16a62dcc54c04347c488e862f
Mikhail_Kuragin <rndmku@gmail.com>
2021-01-22 22:36:38 +0700

    Add Day3 solutions

--- src/main/java/day3/Task1.java
+++ src/main/java/day3/Task1.java
@@ -1,7 +1,39 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String key = scanner.nextLine();
+        while (!key.equalsIgnoreCase("stop")){
+            switch (key){
+                case "Москва":
+                case "Владивосток":
+                case "Ростов":
+                    System.out.println("Россия");
+                    break;
+                case "Рим":
+                case "Милан":
+                case "Турин":
+                    System.out.println("Италия");
+                    break;
+                case "Ливерпуль":
+                case "Манчестер":
+                case "Лондон":
+                    System.out.println("Англия");
+                    break;
+                case "Берлин":
+                case "Мюнхен":
+                case "Кёльн":
+                    System.out.println("Германия");
+                    break;
+                default:
+                    System.out.println("Неизвестная страна");
+            }
+            key = scanner.nextLine();
+        }
+        scanner.close();
 
     }
 }
--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,17 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        double dividend = scanner.nextDouble();
+        double divider = scanner.nextDouble();
+        while (divider != 0){
+            System.out.println(dividend / divider);
+            dividend = scanner.nextDouble();
+            divider = scanner.nextDouble();
+        }
+        scanner.close();
     }
 }
